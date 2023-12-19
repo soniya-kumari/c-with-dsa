ec75bbad3096986d4ef9bb5edd3b430472458b29
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2023-12-19 23:02:43 +0530

    Add files via upload

---
+++ 2darray/search2dmatrix.cpp
@@ -0,0 +1,16 @@
+//search 2d matrix2 leetcode
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int i=0;
+        int j=m-1;
+        while(i<=n-1 && j>=0){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
