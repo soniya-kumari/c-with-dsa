29babdbf497f445e10ca7c05f63c933178dae548
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2023-12-22 23:39:40 +0530

    Add files via upload

---
+++ strings/countconso.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+string str;
+getline(cin,str);
+int n=str.length();
+
+
+    int count=0;
+    int i=0;
+    while(str[i]!='\0'){
+        if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u')
+        count++;
+         i++;
+             
+        
+    }
+
+cout<<count;
+return 0;
+    }
+    
+
