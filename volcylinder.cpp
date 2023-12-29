4e12f2895eba592729fce5720c9015a528ac5285
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2023-12-29 23:26:53 +0530

    Add files via upload

---
+++ volcylinder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int h;
+    cout<<"enter the height of cylinder:"<<endl;
+    cin>>h;
+    int r;
+    cout<<"enter the radius of cylinder:"<<endl;
+    cin>>r;
+    cout<<"volume of cylinder:"<<3.14*r*r*h;
+}
