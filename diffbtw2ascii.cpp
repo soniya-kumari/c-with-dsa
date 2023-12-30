4812e423a1c1117fbdd1c7cdfa7792350c8cb478
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2023-12-30 23:39:47 +0530

    Add files via upload

---
+++ diffbtw2ascii.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch1;
+    cout<<"enter the char1";
+    cin>>ch1;
+    char ch2;
+    cout<<"enter the  char2";
+    cin>>ch2;
+    int x=(int)ch1;
+    int y=(int)ch2;
+    cout<<x-y;
+}
