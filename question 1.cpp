f105770ab047d890476423245afbe51cd4caf4ae
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2023-12-31 22:41:29 +0530

    Add files via upload

---
+++ question 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x;
+cout << "Enter first number\n";
+cin >> x; 
+int y, m;
+cout << "Enter second number and value for taking modulus\n";
+cin >> y >> m; 
+int Z = (x * y) % m;
+cout << "Output is: " << Z;}
+
+//output is:0
