7858f1d9ea5184545042ca2ef351612413a18ecb
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2024-01-06 22:17:36 +0530

    Add files via upload

---
+++ greatestnumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter the first number";
+    int a;
+    cin>>a;
+    cout<<"enter the second number";
+    int b;
+    cin>>b;
+    if(a>b){
+        cout<<"a is greater "<<a<<endl;
+
+    }
+    else{
+        cout<<"b is greater"<<b<<endl;
+    }
+}
