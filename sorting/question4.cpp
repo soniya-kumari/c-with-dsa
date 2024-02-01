b57978e09f84e8fbd2dd36c2a29b0d0c7955e23c
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2024-02-01 21:24:03 +0530

    Add files via upload

---
+++ sorting/question4.cpp
@@ -0,0 +1,19 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+ int arr[5]={8,1,6,9,7};
+ int n=5;
+   for(int i=0;i<n-1;i++){
+           for(int j=0;j<n-i-1;j++){
+               if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+               arr[j] = arr[j + 1];
+               arr[j + 1] = temp;
+                }
+             }
+        }
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        return 0;
+    }
