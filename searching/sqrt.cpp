8125662e81864dc8c8e161d25273c515012b7685
soniya-kumari <142246101+soniya-kumari@users.noreply.github.com>
2024-02-22 23:16:31 +0530

    Add files via upload

---
+++ searching/sqrt.cpp
@@ -0,0 +1,18 @@
+//leetcode question no. 69
+class Solution {
+public:
+    int mySqrt(int x) {
+        int lo=0;
+        int hi=x;
+         while(lo<=hi){
+        int mid= lo+(hi-lo)/2;
+        long long m=  (long long)mid;
+        long long y=(long long)x;
+        if((m*m)==y) return mid;
+       else if((m*m)>x) hi=mid-1;
+      else lo=mid+1;
+         }
+        
+        return hi;
+    }
+};
